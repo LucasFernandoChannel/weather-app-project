d11cb9cbd6f359ca0115f9b03ac2fc5412961bc9
LucasFernandoChannel <131505597+LucasFernandoChannel@users.noreply.github.com>
2023-09-24 18:38:52 +0000

    Add send-telegram-message.mjs file

---
+++ telegram-bot/send-telegram-message.mjs
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch';
+
+const telegramBotToken = '6488991466:AAHFFdOiR6Ln8nVJZyXeho8wdnk3m3hGHaU';
+const chatId = '6622349765';
+
+async function sendTelegramMessage(message) {
+  const url = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
+  const params = {
+    chat_id: chatId,
+    text: message,
+  };
+
+  try {
+    await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params),
+    });
+    console.log('Telegram message sent successfully.');
+  } catch (error) {
+    console.error('Error sending Telegram message:', error.message);
+  }
+}
+
+sendTelegramMessage('Weather web app has been deployed!');
